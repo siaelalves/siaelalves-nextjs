26cb0b8af516b0554d6024065da990e414e70296
Siael Alves <siaelalves@outlook.com>
2025-06-20 14:40:07 -0300

    FIX Corrigido problema que impedia acesso direto a uma URL interna do website e removida a barra no final da URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  output: "export",
- trailingSlash: true, // Optional: adds a trailing slash to the end of URLs
+ trailingSlash: false, // Optional: adds a trailing slash to the end of URLs
   // Other configurations can go here
+rewrites: async () => {
+    return [
+      {
+        source: '/:path/index.html',
+        destination:'/:path',
+      }
+    ];
+  },
+
 };
 
 export default nextConfig;
