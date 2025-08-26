bf9c890d2bfe489400d5cb15ef899d385d833106
Siael Alves <siaelalves@outlook.com>
2025-08-26 16:56:56 -0300

    FIX Finalizado artigo "Como funcionam os planos de hospedagem"

--- src/app/artigos/como-funcionam-os-planos-de-hospedagem/page.js
+++ src/app/artigos/como-funcionam-os-planos-de-hospedagem/page.js
@@ -9,6 +9,9 @@ import gallery from '../../../assets/interface/website-gallery-option.svg'
 import FlatButton from "../../../components/FlatButton/FlatButton"
 import style from './page.module.css'
 import Metadata from "../../../components/Metadata/Metadata"
+import Topic from "@/components/Topic/Topic"
+import ListItem from "@/components/ListItem/ListItem"
+import Link from "next/link"
 
 export const metadata = {
  title: "Como funcionam os planos de hospedagem - Siael Alves",
@@ -28,7 +31,7 @@ export const metadata = {
   title: "Como funcionam os planos de hospedagem - Siael Alves",
   description: "Entenda melhor como os planos de hospedagem funcionam e calcule o custo do seu site antes de criá-lo! Entre em contato comigo para fazer um orçamento",
   type: 'website',
-  publishedTime: '2025-07-14T14:20:00.000Z',
+  publishedTime: '2025-08-26T16:56:00.000Z',
   authors: ["Siael Alves"],
  }
 };
@@ -43,9 +46,57 @@ export default function Page() {
     <Paragraph text="Se você chegou aqui é porque já tem certeza de que quer um site para a sua empresa. Para ajudar 
     você a calcular quanto vai custar criar o seu site, eu preparei esta explicação para você não se perder!"/>
 
-    <Paragraph text="Primeiramente, vamos definir o signifcado de alguns termos que vão ajudar você daqui para frente."/>
+    <Paragraph>De modo simples, um plano de hospedagem funciona como o aluguel de uma loja física. Só que, ao invés 
+     de alugar um espaço físico, você aluga um espaço na internet
+    </Paragraph>
 
-    <Paragraph text="[[[Este artigo está incompleto]]]"/>
+    <Paragraph>Para que a hospedagem do seu site funcione bem, você precisa escolher um bom plano de hospedagem, 
+     com bons servidores e um nome de domínio. Entenda melhor o significado desses termos nos tópicos a seguir</Paragraph>
+
+    <Topic topic={"O que é uma Hospedagem"}/>
+    
+    <Paragraph text="Hospedagem é um serviço de aluguel de servidor que permite que que seu site fique online 
+    24 horas. Dependendo da empresa de hospedagem, é possível conseguir um desconto pagando vários anos adiantados ou 
+    escolher um pagamento mensal"/>
+
+    <Topic topic={"O que é um Servidor"}/>
+    
+    <Paragraph text="Servidor é um computador criado exclusivamente para responder a requisições e enviar dados 
+    para outros computadores. Ele é chamado assim porque ele serve conteúdo para outros computadores"/>
+
+    <Paragraph text="Um serviço de hospedagem utiliza vários servidores para as mais variadas funções. Dentre elas 
+    estão o backup de sites, serviço de e-mail, armazenamento de arquivos, firewalls e muitas outras funções 
+    necessárias"/>
+    
+    <Paragraph text="Por serem muitos computadores, a técnica utilizada para armazenar os arquivos é diferente do que 
+    fazemos em casa. Quanto melhor o serviço de hospedagem, mais potente devem ser esses computadores e mais robustas 
+    as técnicas utilizadas"/>
+
+    <Topic topic={"O que é um Domínio"}/>
+
+    <Paragraph text="Um domínio é o endereço do seu site na web. Para ter um domínio é necessário registrá-lo. Esse 
+    registro pode ser feito por você ou pela empresa de hospedagem. O importante é que ele seja feito no seu nome 
+    ou no nome da sua empresa para evitar problemas mais tarde"/>
+
+    <Paragraph>Para domínios .com, há várias empresas que podem fazer esse registro, como GoDaddy, Hostgator e 
+     Hostinger. Para domínios .com.br, o registro deve ser feito no site <a className="link" href="https://registro.br/">https://registro.br</a></Paragraph>
+
+    <Paragraph>Um domínio precisa ser renovado todos os anos para permanecer ativo. Por isso, você precisa pagar uma 
+     taxa anual para mantê-lo funcionando e deixar seu site acessível
+    </Paragraph>
+
+    <Topic>Conclusão</Topic>
+
+    <Paragraph>Agora que você entende o básico sobre hospedagens, já é possível iniciar o processo de criação do seu 
+     site.</Paragraph>
+
+    {/* <ListItem items={
+     [
+      <Link className="link" href="/artigos/como-colocar-seu-site-online">Como colocar seu site on-line</Link>
+     ]
+    }/> */}
+
+    <Paragraph>Entre em contato comigo para que eu ajude você nesse processo!</Paragraph>
 
     <div className={style.buttonLine}>
      <FlatButton label={"Quero um Site"} icon="socialwhatsapp-icon-dark" path={WhatsAppLink} title={"Entrar em contato pelo WhatsApp"} type="primary"/>
