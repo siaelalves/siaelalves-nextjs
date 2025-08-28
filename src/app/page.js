af1f8edf50209c1072d719d8fbb83d63c57cc483
Siael Alves <siaelalves@outlook.com>
2025-08-28 11:33:05 -0300

    FEAT Adcionados dados estruturados do tipo Person na Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+import Schema from '@/components/Schema/Schema';
 import Header from '../components/Header/Header';
 
 export default function Homepage() {
+ const schema = { 
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  name: "Siael Alves",
+  url: "https://siaelalves.com.br",
+  image: "",
+  sameAs: [
+    "https://siaelalves.com.br",
+    "https://github.com/siaelalves"
+  ],
+  jobTitle: "Programador e Web Designer"
+ }
+
  return (
   <>
+   <Schema data={schema}/>
    <Header/>   
   </>
  )
