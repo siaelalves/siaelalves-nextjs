7877f32bdd7feb07bd6b8434592bfcef40b20a22
Siael Alves <siaelalves@outlook.com>
2025-08-29 15:54:54 -0300

    FEAT Adicionadas animações nos elementos desses componentes;

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,42 @@
+'use client'
 import style from './header.module.css'
 import MainTitle from "../MainTitle/MainTitle";
 import FlatButton from '../FlatButton/FlatButton';
 
+import { motion } from "framer-motion"
+export const Motion = { name: motion.h2, info: motion.div, line: motion.div, buttonLine: motion.div }
+
 export default function Header() {
+ const fadeIn = {
+  hidden: {
+   opacity: 0
+  },
+  visible: {
+   opacity: 1
+  }
+ }
+
+ const draw = {
+  hidden: {
+   width: 0
+  },
+  visible: {
+   width: "75vw"
+  }
+ }
+
  return (
   <header className={style.header}>    
    <div className={style.title}>
-    <h2 className={style.name}>SIAEL ALVES</h2>
+    <Motion.name className={style.name} initial="hidden" animate="visible" variants={fadeIn}>SIAEL ALVES</Motion.name>
     <MainTitle/>
    </div>
-   <div className={style.line}></div>
-   <div className={style.info}>Sites Institucionais, Landing Pages, Blogs e Catálogos On-line</div>
-   <div className={style.buttonLine}>
+   <Motion.line className={style.line} initial="hidden" animate="visible" variants={draw}></Motion.line>
+   <Motion.info className={style.info} initial="hidden" animate="visible" variants={fadeIn}>Sites Institucionais, Landing Pages, Blogs e Catálogos On-line</Motion.info>
+   <Motion.buttonLine className={style.buttonLine} initial="hidden" animate="visible" variants={fadeIn}>
     <FlatButton type={"primary"} label={"Conheça meu trabalho"} path={"/quem-sou-eu"} title="Ir para próxima página"/>
     <FlatButton type={"terciary"} label={"Que site devo escolher"} path={"/duvidas/conheca-os-diferentes-tipos-de-site"} title="Ler artigo Conheça os diferentes tipos de site"/>
-   </div>
+   </Motion.buttonLine>
    
    {/* <img  ={style.background} src="/src/assets/background/header-background-1.jpg"/> */}
   </header>
--- src/components/MainTitle/MainTitle.jsx
+++ src/components/MainTitle/MainTitle.jsx
@@ -1,12 +1,23 @@
 import style from './mainTitle.module.css'
+import { motion } from "framer-motion"
+export const Motion = { title: motion.h1}
 
 export default function MainTitle() {
+ const fadeIn = {
+  hidden: {
+   opacity: 0
+  },
+  visible: {
+   opacity: 1
+  }
+ }
+ 
  return (
   <>
-   <h1 className={style.title}>
+   <Motion.title initial="hidden" animate="visible" variants={fadeIn} className={style.title}>
     <div className={style.line}>Programador &</div>
     <div className={style.line + " " + style.second}>Web Designer</div>
-   </h1>
+   </Motion.title>
   </>
  )
 }
